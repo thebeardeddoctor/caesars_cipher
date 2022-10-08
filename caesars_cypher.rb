07e65f3677dfb1f99f277234be86128a36b412d9
Antonio <beardeddoctor29@gmail.com>
2022-10-07 20:44:20 -0500

    Test method with new shift factor

--- caesars_cypher.rb
+++ caesars_cypher.rb
@@ -3,4 +3,4 @@ def cipher(text, val = 13, alphabet_1 = [*'A'..'Z'], alphabet_2 = [*'a'..'z'])
  lower_case_text = upper_case_text.tr(alphabet_2.join, alphabet_2.rotate(val).join)
 end
 
-puts cipher("What's good Fam?!", 6)
+puts cipher("What's good Fam?!", 7)
