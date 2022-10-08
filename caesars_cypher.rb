215e360cb7090348fb3faa5640503c9117d743e9
Antonio Smith <100506016+thebeardeddoctor@users.noreply.github.com>
2022-10-07 21:37:38 -0400

    Add file via upload

---
+++ caesars_cypher.rb
@@ -0,0 +1,6 @@
+def cipher(text, val = 13, alphabet_1 = [*'A'..'Z'], alphabet_2 = [*'a'..'z'])
+ upper_case_text = text.tr(alphabet_1.join, alphabet_1.rotate(val).join)
+ lower_case_text = upper_case_text.tr(alphabet_2.join, alphabet_2.rotate(val).join)
+end
+
+puts cipher("What's good Fam?!", 5)
